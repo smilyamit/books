6e534de0d60d1b249c92fdbfc267ea7c0e0cf894
Amit Tiwari <tiwari@solandeo.com>
2023-06-03 17:20:16 +0200

    added math based isqrt method

--- random1.py
+++ random1.py
@@ -1,8 +1,10 @@
 '''This is just for testing purposes'''
+#prime number program
+import math
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(2, int(num**0.5) + 1):
+    for i in range(2, int(math.isqrt(num)) + 1):
         if num % i == 0:
             return False
     return True
