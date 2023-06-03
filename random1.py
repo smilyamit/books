87cb483f1938b4d1fc241ea1fe33d714446a8fb9
Amit Tiwari <tiwari@solandeo.com>
2023-06-03 17:16:09 +0200

    just on prime number

--- random1.py
+++ random1.py
@@ -1,1 +1,12 @@
 '''This is just for testing purposes'''
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+for num in range(2, 101):
+    if is_prime(num):
+        print(num)
